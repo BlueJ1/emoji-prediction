6944686677d4c1ba9c63c07d62d68164c759b690
BlueJ1 <l.august@posteo.net>
2023-11-17 20:19:27 +0100

    Parsing to pandas dataframe should work

---
+++ emoji-prediction/data_preprocessing/test_preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pathlib import Path
+
+from parse_to_df import parse_to_df
+
+data_path = Path(__file__).parent.parent / 'data'
+file_name = 'train.txt'
+df = parse_to_df(data_path=data_path, size_to_read=5 * 1024 ** 2)
+print(df.dtypes)
+df.to_csv(data_path / 'train.csv')
