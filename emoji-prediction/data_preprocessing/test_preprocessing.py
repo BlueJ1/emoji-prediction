73b8c17ea1a8c8d7aff0befbf60cf4e4c11595df
BlueJ1 <l.august@posteo.net>
2023-11-21 15:39:46 +0100

    Removed LFS files

--- emoji-prediction/data_preprocessing/test_preprocessing.py
+++ emoji-prediction/data_preprocessing/test_preprocessing.py
@@ -4,7 +4,13 @@ from pathlib import Path
 from parse_to_df import parse_to_df
 
 data_path = Path(__file__).parent.parent / 'data'
+emoji_path = data_path / 'emojis.txt'
 file_name = 'train.txt'
 df = parse_to_df(data_path=data_path, size_to_read=5 * 1024 ** 2)
-print(df.dtypes)
-df.to_csv(data_path / 'train.csv')
+
+with open(emoji_path, 'r') as f:
+    emoji_vocab = {w[:-1]: i for i, w in enumerate(f.readlines())}
+
+# count occurrences of emojis
+emoji_counts = df['sequence_emojis'].apply(pd.Series).stack().value_counts()
+print(emoji_counts)
