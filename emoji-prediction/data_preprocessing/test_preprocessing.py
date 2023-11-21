ccd5318d53bfe79b36a56206fd004bc51fc7b2b5
BlueJ1 <l.august@posteo.net>
2023-11-21 16:41:36 +0100

    We are now ignoring tokens that are not in the glove vocabulary, and their associated emojis are not considered as well.

--- emoji-prediction/data_preprocessing/test_preprocessing.py
+++ emoji-prediction/data_preprocessing/test_preprocessing.py
@@ -1,16 +1,29 @@
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 from parse_to_df import parse_to_df
 
 data_path = Path(__file__).parent.parent / 'data'
 emoji_path = data_path / 'emojis.txt'
 file_name = 'train.txt'
-df = parse_to_df(data_path=data_path, size_to_read=5 * 1024 ** 2)
+df = parse_to_df(data_path=data_path, size_to_read=500 * 1024 ** 2)
 
 with open(emoji_path, 'r') as f:
     emoji_vocab = {w[:-1]: i for i, w in enumerate(f.readlines())}
+    index_to_emoji = {float(i): w for w, i in emoji_vocab.items()}
 
-# count occurrences of emojis
+# count occurrences of top 20 emojis
 emoji_counts = df['sequence_emojis'].apply(pd.Series).stack().value_counts()
+emoji_counts.index = emoji_counts.index.map(index_to_emoji)
+
+# plot
+plt.figure(figsize=(20, 10))
+plt.bar(emoji_counts.index[1:], emoji_counts.values[1:])
+plt.xticks(rotation=90)
+plt.xlabel('Emoji')
+plt.ylabel('Count')
+plt.title('Emoji counts')
+plt.show()
+
 print(emoji_counts)
