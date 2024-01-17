b6e73a1dce85461dda6f2be0233474abf283dad3
LauraMQuiros <l.m.quiros.conesa@student.rug.nl>
2024-01-17 14:23:59 +0100

    Added sbatch files

---
+++ emoji-prediction/sbatch_cross_validation_cpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=30
+
+deactivate
+module purge
+
+module load cuDNN/8.4.1.50-CUDA-11.7.0
+module load Python/3.10.8-GCCcore-12.2.0
+source $HOME/venvs/ep/bin/activate
+which python
+python -V
+
+start=$(date +%s)
+cp -r /scratch/s4776380/emoji-prediction/emoji-prediction/ $TMPDIR/ep/
+end=$(date +%s)
+duration=$((end - start))
+echo "Copying took $duration seconds."
+
+python $TMPDIR/ep/cross_validation.py
